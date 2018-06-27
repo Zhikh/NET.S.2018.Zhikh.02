1eebd964431bab24fb1ba5dd2d78f219078030bf
Domino <zhikhanastasya@gmail.com>
2018-06-27 19:16:39 +0300

    add files(Logic.Task2), add DigitWorker class

---
+++ NET.S.2018.Zhikh.02/Logic.Task2/DigitWorker.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+
+namespace Logic.Task2
+{
+    /// <summary>
+    /// This class gives possibilities for extending work space of digit types.
+    /// </summary>
+    public static class DigitWorker
+    {
+        /// <summary>
+        /// This method inserts bits from two values in new value by indexes.
+        /// </summary>
+        /// <param name="elements"> Elements for searching in </param>
+        /// <param name="checkValue"> Value for searching elements in array </param>
+        /// <returns> List of elements that contain check value </returns>
+        public static List<T> Filter<T>(T[] elements, T checkValue) where T : struct
+        {
+            string checkString = checkValue.ToString();
+
+            var result = new List<T>();
+
+            foreach (var element in elements)
+            {
+                if (element.ToString().Contains(checkString))
+                {
+                    result.Add(element);
+                }
+            }
+
+            return result;
+        }
+    }
+}
