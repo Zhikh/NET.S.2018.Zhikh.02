30d420aa4e1762d9d9feabe9cb4723d4a4332f8b
Domino <zhikhanastasya@gmail.com>
2018-07-01 11:20:01 +0300

    change (Logic.Task2) way of searching numeral

--- NET.S.2018.Zhikh.02/Logic.Task2/DigitWorker.cs
+++ NET.S.2018.Zhikh.02/Logic.Task2/DigitWorker.cs
@@ -15,7 +15,7 @@ namespace Logic.Task2
         /// <param name="checkValue"> Value for searching elements in array </param>
         /// <returns> List of elements that contain check value </returns>
         /// <exception cref="ArgumentNullException"> If elements null </exception>
-        public static T[] Filter<T>(T[] elements, T checkValue) where T : struct
+        public static int[] Filter(int checkValue, params int[] elements)
         {
             if (elements == null)
             {
@@ -24,17 +24,46 @@ namespace Logic.Task2
 
             string checkString = checkValue.ToString();
 
-            var result = new List<T>();
+            var result = new List<int>();
 
             foreach (var element in elements)
             {
-                if (element.ToString().Contains(checkString))
+                if (element.IsContain(checkValue))
                 {
                     result.Add(element);
                 }
             }
 
             return result.ToArray();
+        }
+
+        /// <summary>
+        /// This method checks if checkValue in value.
+        /// </summary>
+        /// <param name="value"> Number for checking </param>
+        /// <param name="checkValue"> Numeral </param>
+        /// <returns> If value contain checkValue return true, else - false </returns>
+        private static bool IsContain(this int value, int checkValue)
+        {
+            int wholePart = value / 10,
+                modulo = value % 10;
+
+            bool result = false;
+            
+            if ((modulo == checkValue) || (wholePart == checkValue))
+            {
+                result = true;
+            }
+            else
+            {
+                if (wholePart != 0)
+                {
+                    result = wholePart.IsContain(checkValue);
+                }
+            }
+
+            return result;
+            
         }
     }
 }
