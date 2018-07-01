d61a682e7ced2f4e58e60b8b3a7e01edf0beee3b
Domino <zhikhanastasya@gmail.com>
2018-07-01 14:22:35 +0300

    chore (Logic.Task2.Tests)

--- NET.S.2018.Zhikh.02/Logic.Task2.Tests/DigitWorkerTests.cs
+++ NET.S.2018.Zhikh.02/Logic.Task2.Tests/DigitWorkerTests.cs
@@ -19,7 +19,7 @@ namespace Logic.Task2.Tests
         [TestMethod]
         [ExpectedException(typeof(ArgumentNullException))]
         public void Filter_WithNull_ThrowArgumentNullException()
-            => DigitWorker.Filter(null, 1);
+            => DigitWorker.Filter(1, null);
 
         [TestMethod]
         public void Filter_WithIntValues_ReturnExampleResult()
@@ -29,24 +29,11 @@ namespace Logic.Task2.Tests
 
             int[] expected = { 56, 456, 5 };
 
-            int[] actual = DigitWorker.Filter(array, value);
+            int[] actual = DigitWorker.Filter(value, array);
 
             CollectionAssert.AreEqual(expected, actual);
         }
 
-        [TestMethod]
-        public void Filter_WithDoubleValues_ReturnExampleResult()
-        {
-            double[] array = { 14.1, 57.89, 2, 4, 12.3, 65.4, 0.12, 54.3, -32.1, -20.12, 5.1, 10.11 };
-            double value = 0.1;
-
-            double[] expected = { 0.12, -20.12, 10.11 };
-
-            double[] actual = DigitWorker.Filter(array, value);
-
-            CollectionAssert.AreEqual(expected, actual);
-        }
-        
         [DataSource("System.Data.SqlClient", @"Data Source=DESKTOP-7HKSO63\SQLEXPRESS;Initial Catalog=NET.S.2018.Zhikh.02;integrated security=True;Pooling=False", "FilterTestData", DataAccessMethod.Sequential)]
         [TestMethod]
         public void Filter_FromDataSource_ReturnDataSourceResult()
@@ -60,7 +47,7 @@ namespace Logic.Task2.Tests
                 string expectedFromDb = TestContext.DataRow["Expected"].ToString();
                 int[] expected = ParseToIntArray(expectedFromDb);
 
-                int[] actual = DigitWorker.Filter(elements, value);
+                int[] actual = DigitWorker.Filter(value, elements);
 
                 CollectionAssert.AreEqual(expected, actual);
             }
