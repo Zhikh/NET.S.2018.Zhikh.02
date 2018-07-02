545dacf5abb015c6e2a73905d0b5719afd4c53d0
Domino <zhikhanastasya@gmail.com>
2018-07-02 23:53:44 +0300

    add (Logic.Task2.NUnitTests) additional tests

--- NET.S.2018.Zhikh.02/Logic.Task2.NUnitTests/DigitWorkerTests.cs
+++ NET.S.2018.Zhikh.02/Logic.Task2.NUnitTests/DigitWorkerTests.cs
@@ -11,11 +11,47 @@ namespace Logic.Task2.NUnitTests
             => Assert.Throws<ArgumentNullException>(() => DigitWorker.FilterDivision(1, null));
 
         [Test]
-        public void Filter_WithExampleValues_ReturnExampleResult()
+        public void Filter_WithExampleValues_ReturnExprctedArray()
         {
             int[] array = { 7, 1, 2, 3, 4, 5, 6, 7, 68, 69, 70, 15, 17 };
             int value = 7;
             int[] expected = { 7, 7, 70, 17 };
+
+            int[] actual = DigitWorker.FilterDivision(value, array);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Filter_WithNegativeValue_ReturnExprctedArray()
+        {
+            int[] array = { -7, 1, 2, 3, 5, 6, 7, 68, 69, 70, 15, 17 };
+            int value = 7;
+            int[] expected = { -7, 7, 70, 17 };
+
+            int[] actual = DigitWorker.FilterDivision(value, array);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Filter_WithLongValues_ReturnExprctedArray()
+        {
+            int[] array = { -700, 551, 20005, 300, 135, 633389, 7324687, 11, 64569, 70, 15, 173333, 112 };
+            int value = 5;
+            int[] expected = { 551, 20005, 135, 64569, 15 };
+
+            int[] actual = DigitWorker.FilterDivision(value, array);
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+
+        [Test]
+        public void Filter_WithoutValue_ReturnEmptyArray()
+        {
+            int[] array = { 7, 1, 2, 3, 5, 6, 7, 68, 69, 70, 15, 17 };
+            int value = 4;
+            int[] expected = { };
 
             int[] actual = DigitWorker.FilterDivision(value, array);
 
