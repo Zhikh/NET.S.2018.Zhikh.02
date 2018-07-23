41520be9cf9bc08b081ed13b9a45949b270bc356
Domino <zhikhanastasya@gmail.com>
2018-07-23 21:37:07 +0300

    Add files

---
+++ NET.S.2018.Zhikh.02/Logic.Task2/IContain.cs
@@ -0,0 +1,7 @@
+﻿namespace Logic.Task2
+{
+    public interface IContain<in T>
+    {
+        bool IsContain(T element, T value);
+    }
+}
---
+++ NET.S.2018.Zhikh.02/Logic.Task2/IntContain.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Logic.Task2
+{
+    public class IntContain : IContain<int>
+    {
+        /// <summary>
+        /// This method checks if checkValue in value.
+        /// </summary>
+        /// <param name="value"> Number for checking </param>
+        /// <param name="checkValue"> Numeral </param>
+        /// <returns> If value contain checkValue return true, else - false </returns>
+        public bool IsContain(int value, int checkValue)
+        {
+            value = Math.Abs(value);
+
+            int wholePart = value / 10,
+                modulo = value % 10;
+
+            bool result = false;
+
+            do
+            {
+                if ((modulo == checkValue) || (wholePart == checkValue))
+                {
+                    result = true;
+                }
+                else
+                {
+                    if (wholePart != 0)
+                    {
+                        modulo = wholePart % 10;
+
+                        wholePart /= 10;
+                    }
+                }
+            }
+            while ((!result) && (wholePart != 0));
+
+            return result;
+        }
+    }
+}
