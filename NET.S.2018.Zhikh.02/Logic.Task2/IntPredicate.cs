8a4328870f05cc83646ffe4b0754b2efaef5c590
Domino <zhikhanastasya@gmail.com>
2018-07-25 09:55:30 +0300

    Add file

---
+++ NET.S.2018.Zhikh.02/Logic.Task2/IntPredicate.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+namespace Logic.Task2
+{
+    public class IntPredicate : IPredicate<int>
+    {
+        private int _checkValue;
+
+        public IntPredicate(int checkValue)
+        {
+            _checkValue = checkValue;
+        }
+
+        /// <summary>
+        /// This method checks if checkValue in value.
+        /// </summary>
+        /// <param name="value"> Number for checking </param>
+        /// <param name="_checkValue"> Numeral </param>
+        /// <returns> If value contain checkValue return true, else - false </returns>
+        public bool IsMatch(int value)
+        {
+            value = Math.Abs(value);
+
+            int wholePart = value / 10,
+                modulo = value % 10;
+
+            bool result = false;
+
+            do
+            {
+                if ((modulo == _checkValue) || (wholePart == _checkValue))
+                {
+                    result = true;
+                }
+                else
+                {
+                    if (wholePart != 0)
+                    {
+                        modulo = wholePart % 10;
+
+                        wholePart /= 10;
+                    }
+                }
+            }
+            while ((!result) && (wholePart != 0));
+
+            return result;
+        }
+    }
+}
