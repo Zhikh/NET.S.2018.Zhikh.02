247367866d08ea25fde71a7d1cadee1fa0f09d8b
Domino <zhikhanastasya@gmail.com>
2018-07-25 10:17:20 +0300

    Add files

---
+++ NET.S.2018.Zhikh.02/Logic.Task2/IEnumerableExtension.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Logic.Task2
+{
+    /// <summary>
+    /// This class gives possibilities for extending work space of digit types.
+    /// </summary>
+    public static class IEnumerableExtension
+    {
+        #region Public methods
+        /// <summary>
+        /// This method finds all numbers that contain checkValue from array of elements (for search it uses division).
+        /// </summary>
+        /// <param name="elements"> Elements for searching in </param>
+        /// <param name="predicate"> Rule for checking </param>
+        /// <returns> List of elements that contain check value </returns>
+        /// <exception cref="ArgumentNullException"> If elements null </exception>
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> elements, IPredicate<T> predicate)
+        {
+            if (elements == null)
+            {
+                throw new ArgumentNullException("Argument elements can't benull!");
+            }
+
+            foreach (var element in elements)
+            {
+                if (predicate.IsMatch(element))
+                {
+                    yield return element;
+                }
+            }
+        }
+
+        /// <summary>
+        /// This method finds all numbers that contain checkValue from array of elements (for search it uses division).
+        /// </summary>
+        /// <param name="elements"> Elements for searching in </param>
+        /// <param name="checkValue"> Value for searching elements in array </param>
+        /// <param name="predicate"> Rule for checking </param>
+        /// <returns> List of elements that contain check value </returns>
+        /// <exception cref="ArgumentNullException"> If elements null </exception>
+        public static IEnumerable<T> Filter<T>(this IEnumerable<T> elements, Predicate<T> predicate)
+        {
+            return elements.Filter(new Nested<T>(predicate));
+        }
+        #endregion
+
+        #region Private methods
+        private class Nested<T> : IPredicate<T>
+        {
+            private Predicate<T> _callback;
+
+            public Nested(Predicate<T> callback)
+            {
+                _callback = callback;
+            }
+
+            public bool IsMatch(T value)
+            {
+                return _callback(value);
+            }
+        }
+        #endregion
+    }
+}
---
+++ NET.S.2018.Zhikh.02/Logic.Task2/StringPredicate.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Logic.Task2
+{
+    public class StringPredicate : IPredicate<string>
+    {
+        private string _checkString;
+
+        /// <summary>
+        /// Initialize StringPredicate object
+        /// </summary>
+        /// <param name="checkValue"> Value for checking </param>
+        public StringPredicate(string checkValue)
+        {
+            if (string.IsNullOrEmpty(checkValue))
+            {
+                throw new ArgumentException($"The {nameof(checkValue)} can't be null or empty!");
+            }
+
+            _checkString = checkValue;
+        }
+
+        /// <summary>
+        /// Check the next: value is consist checkValue
+        /// </summary>
+        /// <param name="value"> Value for checking </param>
+        /// <returns> If value has checkValue, it will be true, else - false </returns>
+        public bool IsMatch(string value)
+        {
+            if (string.IsNullOrEmpty(value))
+            {
+                throw new ArgumentException($"The {nameof(value)} can't be null or empty!");
+            }
+
+            return value.ToString().Contains(_checkString);
+        }
+    }
+}
